7bef4c1f423ea0fec1f5b83cc17c5f1eb41a1303
William Otsuka <william-ots@hotmail.com>
2024-06-08 00:42:22 -0300

    Adição do feedback, Create das tabelas e tabela de denuncias. Acredito eu que so falte o formulario de enviar denuncias

---
+++ Ativo e Operante/AtivoOperante2024/src/main/java/br/unoeste/fipp/ativooperante2024/db/repositories/FeedbackRepository.java
@@ -0,0 +1,2 @@
+package br.unoeste.fipp.ativooperante2024.db.repositories;public class FeedbackRepository {
+}
---
+++ Ativo e Operante/AtivoOperante2024/src/main/java/br/unoeste/fipp/ativooperante2024/services/FeedbackService.java
@@ -0,0 +1,2 @@
+package br.unoeste.fipp.ativooperante2024.services;public class FeedbackService {
+}
